d138932d1676e4bba44d74bdc28cf1d13eedaf57
IKreike <iris.kreike@hva.nl>
2025-02-06 16:25:04 +0100

    6/2/25
    reverse flashlight
    lantern

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
 console.log("hello world")
+
+// follow along flashlight source
+// https://codepen.io/bramus/pen/eBZgPB
+const root = document.documentElement;
+
+document.addEventListener('mousemove', evt => {
+    let x = evt.clientX / innerWidth;
+    let y = evt.clientY / innerHeight;
+ 
+    root.style.setProperty('--mouse-x', x);
+    root.style.setProperty('--mouse-y', y);
+});
