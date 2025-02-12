5153e08873014ce02a9859db5f822763b26ae86d
IKreike <iris.kreike@hva.nl>
2025-02-12 16:29:21 +0100

    12/2/25
    Big swing lanterns
    Click functionality to display info
    Updated text and added in pictures

--- scripts/script.js
+++ scripts/script.js
@@ -12,14 +12,22 @@ const MyURL = baseURL+endpointMe;
 console.log(MyURL);
 
 getData(MyURL).then(  IrisData => {
-	// console.log(IrisData.data);
+	console.log(IrisData.data);
 
 	// Zorg dat de zelf ingevulde data makkelijk bereikbaar is
 	let CustomData = JSON.parse(IrisData.data.custom);
-	// console.log(CustomData.LeerDoel1);
+	console.log(CustomData);
 
 	// set variable myName als de naam uit de database 
-	let myName = IrisData.data.name;
+	// let personInfo1 = document.querySelector("#personInfo1");
+	// personInfo1.textContent = IrisData.data.name;
+	let personInfo2 = document.querySelector("#personInfo2");
+	personInfo2.textContent = IrisData.data.name;
+	let personInfo3 = document.querySelector("#personInfo3");
+	personInfo3.textContent = IrisData.data.website;
+	// let personInfo4 = document.querySelector("#personInfo4");
+	// personInfo4.textContent = IrisData.data.name;
+	// let myName = IrisData.data.name;
 
 	let Goal1 = document.querySelector("#goal1");
 	Goal1.textContent = CustomData.Leerdoel1;
@@ -29,6 +37,16 @@ getData(MyURL).then(  IrisData => {
 	Goal3.textContent = CustomData.Leerdoel3;
 	let Goal4 = document.querySelector("#goal4");
 	Goal4.textContent = CustomData.Leerdoel4;
+	let Goal5 = document.querySelector("#goal5");
+	Goal5.textContent = CustomData.Leerdoel6;
+
+	let Info1 = document.querySelector("#Info1");
+	Info1.textContent = CustomData.Info1;
+	let Info2 = document.querySelector("#Info2");
+	Info2.textContent = CustomData.Info2;
+	let Info3 = document.querySelector("#Info3");
+	Info3.textContent = CustomData.Info3;
+
 }  );
 
 async /*9*/ function getData(URL) {
@@ -69,4 +87,22 @@ function StartChase(){
     console.log("clicked");
     TopMask.classList.add("spotlightAnimation")
     body.classList.add("active");
-}
+	body.classList.add("bigSwing");
+	setTimeout(removeAnimation, 5000);
+
+	// only edited out for testing
+	lantern1.removeEventListener("click", StartChase)
+};
+
+function removeAnimation(){
+	body.classList.remove("bigSwing");
+};
+
+// setTimeout(() => {
+// 	document.body.classList.add('trigger'); // Trigger Animation B
+  
+// 	setTimeout(() => {
+// 	  document.body.classList.remove('trigger'); // Remove class after 10s
+// 	}, 10000); // Matches Animation B's duration
+//   }, 5000);
+  
